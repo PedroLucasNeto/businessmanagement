e9da9f17d7c4495af8e38d20d75dfadff8339ad8
Pedro Lucas <pedrolukasneto@gmail.com>
2024-06-30 09:07:00 -0300

    feat: clients listing

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -121,6 +121,8 @@ function processValue (type, value) {
   if (type === 'date') return new Date(value).toLocaleDateString()
   if (type === 'dateTime') return new Date(value).toLocaleString()
   if (type === 'money') return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value)
+  if (type === 'hour') return `${value} horas`
+  if (type === 'phone') return `(${value.slice(0, 2)}) ${value.slice(2, 7)}-${value.slice(7)}`
   return value
 }
 </script>
--- src/pages/management/components/client/Client.vue
+++ src/pages/management/components/client/Client.vue
@@ -1,12 +1,25 @@
 <template>
-    {{ clients }}
+    <Table :tableData="clients" :tableFields="fields" :perPage="10" :quantity="clients.length || 0" />
 </template>
 
 <script setup>
+import Table from '@/components/Table.vue';
 import clientService from '@/api/services/clientService'
 import { onMounted, ref } from 'vue';
 
 const clients = ref([]);
+// { "dateOfBirth": "1991-06-19", "email": "dih_adriana@hotmail.com", "favorite": false, "id": 77, "instagram": "", "name": "Adriana Sousa da silva", "occupation": "Cuidadora de Idosos", "phone": "83988623764", "status": true }
+
+const fields = [
+    { label: 'ID', value: 'id', type: 'number' },
+    { label: 'Nome', value: 'name', type: 'string' },
+    { label: 'Email', value: 'email', type: 'string' },
+    { label: 'Telefone', value: 'phone', type: 'string' },
+    { label: 'Data de Nascimento', value: 'dateOfBirth', type: 'date' },
+    { label: 'Ocupação', value: 'occupation', type: 'string' },
+    { label: 'Instagram', value: 'instagram', type: 'string' },
+    { label: 'Status', value: 'status', type: 'boolean' },
+]
 
 async function getClients () {
     const clientsFromDb = await clientService.getAllClients();
