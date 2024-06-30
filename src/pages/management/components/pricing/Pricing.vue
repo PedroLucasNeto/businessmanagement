c4a79fa476033689d67c4e59baba4d3d87d64aaa
Pedro Lucas <pedrolukasneto@gmail.com>
2024-06-30 08:59:45 -0300

    feat: pricings listing

--- src/pages/management/components/pricing/Pricing.vue
+++ src/pages/management/components/pricing/Pricing.vue
@@ -1,12 +1,20 @@
 <template>
-    {{ pricings }}
+    <Table :tableData="pricings" :tableFields="fields" :perPage="10" :quantity="pricings.length || 0" />
 </template>
 
 <script setup>
+import Table from '@/components/Table.vue';
 import pricingService from '@/api/services/pricingService'
 import { onMounted, ref } from 'vue';
 
 const pricings = ref([]);
+
+const fields = [
+    { label: 'ID', value: 'id', type: 'number' },
+    { label: 'Nome', value: 'description', type: 'string' },
+    { label: 'Duração', value: 'duration', type: 'hour' },
+    { label: 'Valor', value: 'price', type: 'money' },
+]
 
 async function getPricings () {
     const pricingsFromDb = await pricingService.getAllPricings();
